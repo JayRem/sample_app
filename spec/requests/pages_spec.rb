7a2ddcf67d50f89f7b40aa600ee33fb2a23accb6
Jérémy SAFONT <jrm.safont@gmail.com>
2014-07-31 16:22:17 +0200

    Add a full_title helper

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe "Pages" do
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
     it "should have the content 'Sample App'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
@@ -8,10 +10,14 @@ describe "Pages" do
       page.should have_selector('h1', :text => 'Sample App')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/pages/home'
-      page.should have_selector('title',
-                  :text => "Ruby on Rails Tutorial Sample App | Home")
+      page.should have_selector('title', :text => "#{base_title}")
+    end
+
+    it "should not have a custom page title" do
+      visit '/pages/home'
+      page.should_not have_selector('title', :text => '| Home')
     end
   end
 
@@ -24,9 +30,21 @@ describe "Pages" do
 
     it "should have the title 'Contact'" do
       visit '/pages/contact'
-      page.should have_selector('title',
-                                :text => "Ruby on Rails Tutorial Sample App | Contact")
-      end
+      page.should have_selector('title', :text => "#{base_title} | Contact")
+    end
+  end
+
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/pages/help'
+      page.should have_selector('h1', :text => 'Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/pages/help'
+      page.should have_selector('title', :text => "#{base_title} | Help")
+    end
   end
 
   describe "About page" do
@@ -37,8 +55,7 @@ describe "Pages" do
 
     it "should have the title 'About Us'" do
       visit '/pages/about'
-      page.should have_selector('title',
-                                :text => "Ruby on Rails Tutorial Sample App | About Us")
+      page.should have_selector('title', :text => "#{base_title} | About Us")
     end
   end
 end
