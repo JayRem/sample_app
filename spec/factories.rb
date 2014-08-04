dc6efc1bf87b1fae1c9d34addf362e310c4b5512
Jérémy SAFONT <jrm.safont@gmail.com>
2014-08-04 16:50:05 +0200

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name      "Jeremy SAFONT"
+    email     "jeremy.safont@example.com"
+    password  "foobar"
+    password_confirmation "foobar"
+  end
+end
